76824047345b502a53f3c3b19ddf8d3d2ff84226
Spablob <tiagonunespereira@gmail.com>
2023-05-22 14:41:35 +0100

    AevoToken deploy script

---
+++ scripts/deploy/deploy-AevoToken.ts
@@ -0,0 +1,45 @@
+const hre = require("hardhat");
+const { TOKEN_PARAMS } = require("../../params");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+  const network = hre.network.name;
+
+  // We get the contract to deploy
+  const AevoToken = await hre.ethers.getContractFactory("AevoToken", deployer);
+
+  // Use a different name for obfuscation just in case anyone's watching
+  const name = network === "goerli" || network === "sepolia" ? "TestToken" : TOKEN_PARAMS.NAME;
+  const symbol = network === "goerli" || network === "sepolia" ? "TT" : TOKEN_PARAMS.SYMBOL;
+  const beneficiary =
+    network === "goerli" || network === "sepolia" ? deployer.address : TOKEN_PARAMS.BENEFICIARY;
+
+  console.log("name", name);
+  console.log("symbol", symbol);
+  console.log("beneficiary", beneficiary);
+
+  const aevoToken = await AevoToken.deploy(name, symbol, beneficiary);
+
+  await aevoToken.deployed();
+
+  console.log(
+    `\nAevo token is deployed at ${aevoToken.address}, verify with https://etherscan.io/proxyContractChecker?a=${aevoToken.address}\n`
+  );
+
+  await aevoToken.deployTransaction.wait(5);
+
+
+  await hre.run("verify:verify", {
+    address: aevoToken.address,
+    constructorArguments: [name, symbol, beneficiary],
+  });
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
