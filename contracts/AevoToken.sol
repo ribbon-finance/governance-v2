bb2471c4ffb9c3e335f3277bbb11b80b96b45d35
Chuddy <jacob4seinfeld@gmail.com>
2023-05-30 14:48:18 -0700

    Update AevoToken.sol

--- contracts/AevoToken.sol
+++ contracts/AevoToken.sol
@@ -1,11 +1,29 @@
-// SPDX-License-Identifier: GPL-3.0
+/* SPDX-License-Identifier: GPL-3.0 
+
+888       888 888               888    d8b                                                                                 d8b 888    d8b                  .d8888b.  
+888   o   888 888               888    88P                                                                                 Y8P 888    Y8P                 d88P  Y88b 
+888  d8b  888 888               888    8P                                                                                      888                             .d88P 
+888 d888b 888 88888b.   8888b.  888888 "  .d8888b       888  888  .d88b.  888  888 888d888      88888b.   .d88b.  .d8888b  888 888888 888  .d88b.  88888b.   .d88P"  
+888d88888b888 888 "88b     "88b 888       88K           888  888 d88""88b 888  888 888P"        888 "88b d88""88b 88K      888 888    888 d88""88b 888 "88b  888"    
+88888P Y88888 888  888 .d888888 888       "Y8888b.      888  888 888  888 888  888 888          888  888 888  888 "Y8888b. 888 888    888 888  888 888  888  888     
+8888P   Y8888 888  888 888  888 Y88b.          X88      Y88b 888 Y88..88P Y88b 888 888          888 d88P Y88..88P      X88 888 Y88b.  888 Y88..88P 888  888          
+888P     Y888 888  888 "Y888888  "Y888     88888P'       "Y88888  "Y88P"   "Y88888 888          88888P"   "Y88P"   88888P' 888  "Y888 888  "Y88P"  888  888  888     
+                                                             888                                888                                                                  
+                                                        Y8b d88P                                888                                                                  
+                                                         "Y88P"                                 888 
+*/ 
+
 pragma solidity 0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 /**
- * AEVO: STRUCTURED PRODUCTS FOR THE PEOPLE
+ * @title Aevo Token
+ * @notice Governance token of the Aevo exchange, Aevo structured products, and Aevo L2
+ * @dev ERC20 in addition to:
+ *        - EIP-2612 signed approval implementation
+ *        - `mint` functionality by DAO  
  */
 contract Aevo is AccessControl, ERC20Permit {
     /// @dev The identifier of the role which maintains other roles.
