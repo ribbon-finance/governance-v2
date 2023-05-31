95178ddb6c1509792cbaa62e56169acfe4b677c4
Spablob <tiagonunespereira@gmail.com>
2023-05-31 10:51:15 +0100

    remove ReentrancyGuard import

--- contracts/Migrator.sol
+++ contracts/Migrator.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.18;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 /**
  * @title RbnToAevoMigrator
