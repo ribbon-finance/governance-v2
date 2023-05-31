f5e095dd975ee1c5d5a92574aa140efe961d23fd
Spablob <tiagonunespereira@gmail.com>
2023-05-31 12:10:43 +0100

    move addresses from params.js to constants.ts

--- params.js
+++ params.js
@@ -1,6 +1,6 @@
+const {TEST_BENEFICIARY, DAO_MULTISIG} = require("./constants/constants");
+
 const isTest = process.env.CI;
-const TEST_BENEFICIARY = "0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e";
-const DAO_MULTISIG = "0xDAEada3d210D2f45874724BeEa03C7d4BBD41674";
 const beneficiary = isTest ? TEST_BENEFICIARY : DAO_MULTISIG;
 
 // FOR MAINNET
